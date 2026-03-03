b3e37241f34cfc650614fb8a39aaf4cc0cc3fe00
mira <miraslamaj@gmail.com>
2026-03-03 17:28:59 +0200

    define deduplication function

--- openfiles.py
+++ openfiles.py
@@ -1,6 +1,12 @@
 import os
 import json
 
+def de_duplicate(data):
+    #Keep only the last instance with the same "id"
+    unique_data = {tweet["id"]: tweet for tweet in data}.values()
+    # Convert back to list and return result
+    return list(unique_data)
+
 path = r"../apify/digital_ids" #path that includes the .json files (only) 
 
 #Loop through all files in folder and clean them
@@ -13,10 +19,7 @@ for file in os.scandir(path):
             data = json.load(f) # Load .json file for clean up
         
         #De-duplicate json data (remove duplicate tweets by id)
-        unique_data = {tweet["id"]: tweet for tweet in data}.values()
-
-        # Convert back to list
-        unique_data = list(unique_data)
+        unique_data = de_duplicate(data)
 
         # Overwrite file with cleaned data
         with open(file.path, "w", encoding="utf-8") as f:
